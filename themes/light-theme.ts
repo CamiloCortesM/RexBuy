d8e913dded0705f966b4cdce58aabeec16cdb7c4
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-08 12:39:44 -0500

    light theme mui

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -1,16 +1,16 @@
 import { createTheme } from '@mui/material/styles';
-import { red,  } from '@mui/material/colors';
-
+import { red } from '@mui/material/colors';
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '##1E1E1E'
+      main: '#f6bf8c',
     },
     secondary: {
-      main: '#3A64D8'
-    }
+      main: '#f6f1e9',
+    },
+    // main: '#1E1E1E',
   },
   components: {
     MuiLink: {
@@ -26,60 +26,57 @@ export const lightTheme = createTheme({
       styleOverrides: {
         root: {
           backgroundColor: 'white',
-          height: 60
+          height: 60,
         },
-      }
+      },
     },
 
     MuiTypography: {
       styleOverrides: {
         h1: {
           fontSize: 30,
-          fontWeight: 600
+          fontWeight: 600,
         },
         h2: {
           fontSize: 20,
-          fontWeight: 400
+          fontWeight: 400,
         },
         subtitle1: {
           fontSize: 18,
-          fontWeight: 600
-        }
-      }
+          fontWeight: 600,
+        },
+      },
     },
-
-
     MuiButton: {
       defaultProps: {
         variant: 'contained',
         size: 'small',
         disableElevation: true,
+        color: 'info',
       },
       styleOverrides: {
         root: {
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
-          ":hover": {
+          ':hover': {
             backgroundColor: 'rgba(0,0,0,0.05)',
-            transition: 'all 0.3s ease-in-out'
-          }
-        }
-      }
+            transition: 'all 0.3s ease-in-out',
+          },
+        },
+      },
     },
-
 
     MuiCard: {
       defaultProps: {
-        elevation: 0
+        elevation: 0,
       },
       styleOverrides: {
         root: {
           boxShadow: '0px 5px 5px rgba(0,0,0,0.05)',
           borderRadius: '10px',
-        }
-      }
-    }
-    
-  }
+        },
+      },
+    },
+  },
 });
