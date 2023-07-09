f6d5023ed8d962d7278868fed9ddbffd45cb1bbf
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-08 23:11:44 -0500

    Add home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { CssBaseline,ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
-import { lightTheme } from '../themes/light-theme';
+import { lightTheme, darkTheme } from '../themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
+import { NextPage } from 'next';
+import { Typography } from '@mui/material';
 
-export default function Home() {
+import { ShopLayout } from '@/components/layouts/ShopLayout';
+import { initialData } from '../database/products';
+import { ProductList } from '../components/products';
+
+const Home: NextPage = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
-}
+    <ShopLayout
+      title={'RexBuy - Home'}
+      pageDescription={'Encuentra los mejores productos de tecnologia aquí'}
+    >
+      <Typography variant="h1" component="h1" color="primary">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+
+      <ProductList products={initialData.products as any} />
+    </ShopLayout>
+  );
+};
+
+export default Home;
