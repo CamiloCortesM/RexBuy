d36dda77c6a5c24149eb7b85d5bad25441503b25
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-14 02:17:53 -0500

    show page active menu

--- components/ui/Nabvar.tsx
+++ components/ui/Nabvar.tsx
@@ -15,8 +15,11 @@ import {
   ShoppingCartOutlined,
   MenuOutlined,
 } from '@mui/icons-material';
+import { useRouter } from 'next/router';
 
 export const Navbar = () => {
+  const { asPath } = useRouter();
+
   return (
     <AppBar>
       <Toolbar>
@@ -37,37 +40,121 @@ export const Navbar = () => {
         <Box sx={{ display: { xs: 'none', md: 'block' } }}>
           <NextLink href="/category/cellphones" passHref legacyBehavior>
             <Link>
-              <Button>celulares</Button>
+              <Button
+                color={asPath === '/category/cellphones' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/cellphones' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                celulares
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/computers" passHref legacyBehavior>
             <Link>
-              <Button>computadores</Button>
+              <Button
+                color={asPath === '/category/computers' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/computers' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                computadores
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/videogames" passHref legacyBehavior>
             <Link>
-              <Button>videojuegos</Button>
+              <Button
+                color={asPath === '/category/videogames' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/videogames' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                videojuegos
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/accessories" passHref legacyBehavior>
             <Link>
-              <Button>accesorios</Button>
+              <Button
+                color={asPath === '/category/accessories' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/accessories' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                accesorios
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/tablets" passHref legacyBehavior>
             <Link>
-              <Button>tabletas</Button>
+              <Button
+                color={asPath === '/category/tablets' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/tablets' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                tabletas
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/monitors" passHref legacyBehavior>
             <Link>
-              <Button>monitores</Button>
+              <Button
+                color={asPath === '/category/monitors' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/monitors' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                monitores
+              </Button>
             </Link>
           </NextLink>
           <NextLink href="/category/smartwatch" passHref legacyBehavior>
             <Link>
-              <Button>smartwatch</Button>
+              <Button
+                color={asPath === '/category/smartwatch' ? 'primary' : 'info'}
+                sx={{
+                  ...(asPath === '/category/smartwatch' && {
+                    ':hover': {
+                      backgroundColor: 'rgb(98, 45, 24)',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  }),
+                }}
+              >
+                smartwatch
+              </Button>
             </Link>
           </NextLink>
         </Box>
