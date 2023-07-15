970783972809b7edf1c2770d6e610807e3f191fd
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-15 11:44:43 -0500

    create cart context and interfaces

---
+++ interfaces/cart.ts
@@ -0,0 +1,12 @@
+export interface ICartProduct {
+  _id: string;
+  images: string;
+  price: number;
+  slug: string;
+  title: string;
+  brand: string;
+  model: string;
+  capacity?: string;
+  ram?: string;
+  quantity: number;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { SWRConfig } from 'swr';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 import { lightTheme, darkTheme } from '../themes';
-import { UiProvider } from '@/context';
+import { CartProvider, UiProvider } from '@/context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,12 +14,14 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <UiProvider>
-        <ThemeProvider theme={lightTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UiProvider>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
     </SWRConfig>
   );
 }
