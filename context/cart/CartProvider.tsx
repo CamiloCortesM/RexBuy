95693133d34d140dd3a72ec091cfa46f81ed4048
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-07-15 17:55:47 -0500

    Calculate amounts payable

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -35,6 +35,28 @@ export const CartProvider: FC<PropsWithChildren> = ({ children }) => {
     Cookie.set('cart', JSON.stringify(state.cart));
   }, [state.cart]);
 
+  useEffect(() => {
+    const numberOfItems = state.cart.reduce(
+      (prev, current) => current.quantity + prev,
+      0
+    );
+    const subTotal = state.cart.reduce(
+      (prev, current) => current.quantity * current.price + prev,
+      0
+    );
+
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+
+    const OrderSummary = {
+      numberOfItems,
+      subTotal,
+      tax: subTotal * taxRate,
+      total: subTotal * (taxRate + 1),
+    };
+
+    console.log(OrderSummary);
+  }, [state.cart]);
+
   const addProductToCart = (product: ICartProduct) => {
     const productInCart = state.cart.some((p) => p._id === product._id);
     if (!productInCart)
