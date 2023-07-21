2524dc50a980e3834e17d41e49343d3bbdcb9a96
DANIELSSF <daniel.santiago730@gmail.com>
2023-07-21 10:21:36 -0500

    Implements AuthContext to handle authentication state

---
+++ context/auth/AuthContext.tsx
@@ -0,0 +1,12 @@
+import { IUser } from '@/interfaces';
+import { createContext } from 'react';
+
+interface ContextProps {
+  isLoggedIn: boolean;
+  user?: IUser;
+
+  loginUser: (email: string, password: string) => Promise<boolean>;
+  registerUser: (email: string, name: string, password: string) => Promise<{  hasError: boolean;message?: string; }>;
+}
+
+export const AuthContext = createContext({} as ContextProps);
