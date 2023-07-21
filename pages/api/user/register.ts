524eafd6b148ff81ce14945b1cc6c6bab68cebae
DANIELSSF <daniel.santiago730@gmail.com>
2023-07-21 10:25:11 -0500

    Add register page endpoint

---
+++ pages/api/user/register.ts
@@ -0,0 +1,99 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcryptjs from 'bcryptjs';
+
+import { db } from '@/database';
+import { User } from '@/models';
+import { jwt, validations } from '@/utils';
+
+type Data =
+  | { message: string }
+  | {
+      token: string;
+      user: {
+        email: string;
+        role: string;
+        name: string;
+      };
+    };
+
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  switch (req.method) {
+    case 'POST':
+      return registerUser(req, res);
+
+    default:
+      res.status(400).json({
+        message: 'Bad request',
+      });
+  }
+};
+
+const registerUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const {
+    password = '',
+    email = '',
+    name = '',
+  } = req.body as { password: string; email: string; name: string };
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'Incorrect password, should be more than 6 characters long',
+    });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({
+      message: 'Name is too short',
+    });
+  }
+
+  if (!validations.isValidEmail(email)) {
+    return res.status(400).json({
+      message: 'Invalid email',
+    });
+  }
+
+  await db.connect();
+  const user = await User.findOne({ email });
+
+  if (user) {
+    await db.disconnect();
+    return res.status(400).json({
+      message: 'Email in use',
+    });
+  }
+
+  const newUser = new User({
+    name,
+    email: email.toLocaleLowerCase(),
+    password: bcryptjs.hashSync(password),
+    role: 'client',
+  });
+
+  const { role, _id } = newUser;
+
+  try {
+    await newUser.save({ validateBeforeSave: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Talk to the administrator',
+    });
+  }
+
+  const token = jwt.signJWT(_id, email);
+
+  res.status(200).json({
+    token,
+    user: {
+      email,
+      role,
+      name,
+    },
+  });
+};
+
+export default handler;
