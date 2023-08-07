f7d1682a74f65cc7009b0c1290053e089d361d44
DANIELSSF <daniel.santiago730@gmail.com>
2023-08-06 21:31:47 -0500

    Implement Authentication Middleware for address and summary pages

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,21 @@
 import { NextResponse, type NextRequest } from 'next/server';
-import * as jose from 'jose';
+import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const previousPage = req.nextUrl.pathname;
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_URL });
 
-  if (previousPage.startsWith('/checkout')) {
-    const token = req.cookies.get('token')?.value;
-    if (!token) {
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${previousPage}`, req.url)
-      );
-    }
-    try {
-      await jose.jwtVerify(
-        token,
-        new TextEncoder().encode(process.env.JWT_SECRET_SEED)
-      );
-      return NextResponse.next();
-    } catch (error) {
-      console.log(error);
-      return NextResponse.redirect(
-        new URL(`/auth/login?p=${previousPage}`, req.url)
-      );
-    }
+  if (!session) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = '/auth/login';
+    url.search = `p=${requestedPage}`;
+
+    return NextResponse.redirect(url);
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/checkout/:path*'],
+  matcher: ['/checkout/address', '/checkout/summary'],
 };
