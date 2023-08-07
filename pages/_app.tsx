4bfa3e89a29362d235e53f2eac1f8aec05543860
DANIELSSF <daniel.santiago730@gmail.com>
2023-08-06 21:32:50 -0500

    Add SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,33 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { SWRConfig } from 'swr';
+import { SessionProvider } from 'next-auth/react';
+
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SWRConfig } from 'swr';
 
 import { lightTheme, darkTheme } from '../themes';
 import { AuthProvider, CartProvider, UiProvider } from '@/context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (resource, init) =>
-          fetch(resource, init).then((res) => res.json()),
-      }}
-    >
-      <AuthProvider>
-        <CartProvider>
-          <UiProvider>
-            <ThemeProvider theme={lightTheme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </UiProvider>
-        </CartProvider>
-      </AuthProvider>
-    </SWRConfig>
+    <SessionProvider>
+      <SWRConfig
+        value={{
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((res) => res.json()),
+        }}
+      >
+        <AuthProvider>
+          <CartProvider>
+            <UiProvider>
+              <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+              </ThemeProvider>
+            </UiProvider>
+          </CartProvider>
+        </AuthProvider>
+      </SWRConfig>
+    </SessionProvider>
   );
 }
