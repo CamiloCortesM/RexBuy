a2569788f563a3060c867a5ed5f3ff32f456e05d
DANIELSSF <daniel.santiago730@gmail.com>
2023-08-12 19:46:40 -0500

    Add navigation functionality to Order History link in ComponentName

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -90,7 +90,7 @@ export const SideMenu = () => {
                 <ListItemText primary={'Perfil'} />
               </ListItemButton>
 
-              <ListItemButton>
+              <ListItemButton onClick={()=> navigateTo('/orders/history')}>
                 <ListItemIcon>
                   <ConfirmationNumberOutlined />
                 </ListItemIcon>
