5f0b114603830fc8d5629aaee6a0837d4c72ae6e
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-08-19 19:03:03 -0500

    improve speed in the dashboard endpoint

--- pages/api/admin/dashboard.ts
+++ pages/api/admin/dashboard.ts
@@ -1,6 +1,8 @@
 import { IOrder, IProduct, IUser } from '@/interfaces';
 import { Order, Product, User } from '@/models';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { connect } from '../../../database/db';
+import { db } from '@/database';
 
 type Data = {
   numberOfOrders: number;
@@ -16,23 +18,29 @@ export default async function (
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const orders: IOrder[] = await Order.find({});
-  const products: IProduct[] = await Product.find({});
-  const numberOfOrders: number = orders.length;
-  const paidOrders: number = orders.filter((order) => order.isPaid).length;
-  const notPaidOrders: number = orders.filter((order) => order.isPaid).length;
-  const numberOfClients: number = (await User.find({ role: 'client' })).length;
-  const numberOfProducts: number = products.length;
-  const productsWithNoInventory: number = products.filter(
-    (product) => product.inStock === 0
-  ).length;
-  const lowInventory: number = products.filter(
-    (product) => product.inStock <= 10
-  ).length;
+  await db.connect();
+  const [
+    numberOfOrders,
+    paidOrders,
+    numberOfClients,
+    numberOfProducts,
+    productsWithNoInventory,
+    lowInventory,
+  ] = await Promise.all([
+    Order.count(),
+    Order.find({ isPaid: true }).count(),
+    User.find({ role: 'client' }).count(),
+    Product.count(),
+    Product.find({ inStock: 0 }).count(),
+    Product.find({ inStock: { $lte: 10 } }).count(),
+  ]);
+
+  await db.disconnect();
+
   res.status(200).json({
     numberOfOrders,
     paidOrders,
-    notPaidOrders,
+    notPaidOrders: numberOfOrders - paidOrders,
     numberOfClients,
     numberOfProducts,
     productsWithNoInventory,
