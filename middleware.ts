0b9fb4156ff1cbeaf6ff6f5d6d90e411950d5877
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-08-19 23:18:07 -0500

    Middlewares to validate authentication

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,40 @@
 import { NextResponse, type NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
+const createUnauthorizedResponse = () => {
+  return new Response(JSON.stringify({ message: 'Not authorized' }), {
+    status: 401,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
 export async function middleware(req: NextRequest) {
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const previousPage = req.nextUrl.pathname;
+  const validRoles = ['admin', 'employee'];
+
+  const session: any = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
 
   if (!session) {
-    const requestedPage = req.nextUrl.pathname;
-    const url = req.nextUrl.clone();
-    url.pathname = '/auth/login';
-    url.search = `p=${requestedPage}`;
+    if (previousPage.startsWith('/api/admin'))
+      return createUnauthorizedResponse();
+    const url = new URL('/auth/login', req.nextUrl.origin);
+    url.search = `p=${previousPage}`;
+    return NextResponse.redirect(url);
+  }
 
+  if (
+    (previousPage.startsWith('/admin') ||
+      previousPage.startsWith('/api/admin')) &&
+    !validRoles.includes(session.user.role)
+  ) {
+    if (previousPage.startsWith('/api/admin'))
+      return createUnauthorizedResponse();
+    const url = new URL('/', req.nextUrl.origin);
     return NextResponse.redirect(url);
   }
 
@@ -17,6 +42,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/checkout/address', '/checkout/summary'],
+  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*'],
 };
-
