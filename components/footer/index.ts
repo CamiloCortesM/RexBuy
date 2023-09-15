5a79e0b3d48aedc955f7b202881aaa422e05a10d
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-15 14:55:01 -0500

    Create main footer component as container for sub-components

---
+++ components/footer/index.ts
@@ -0,0 +1,7 @@
+export * from './FooterNav';
+export * from './Company';
+export * from './Contacts';
+export * from './PrincipalPage';
+export * from './aboutInfo/About';
+export * from './aboutInfo/ListItemBenefits';
+export * from './aboutInfo/Benefits';
