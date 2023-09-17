2b791cc015d71c77baf7746eeb29554a71770cf4
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-16 19:24:55 -0500

    Refactor: Optimize Code, Add Props, Organize Constants, and Remove Conditional Rendering

--- components/footer/Contacts.tsx
+++ components/footer/Contacts.tsx
@@ -1,97 +1,55 @@
 import { FC, useState } from 'react';
 
-// import Link from 'next/link';
-
 import {
   Box,
-  Button,
   Collapse,
   Divider,
   List,
   ListItemButton,
   ListItemText,
-  useMediaQuery,
 } from '@mui/material';
 
-const developerNames = ['Daniel Silva', 'Santiago Salamanca', 'Camilo Cortes'];
-
-const githubs = [
-  'https://github.com/DANIELSSF',
-  'https://github.com/SantiagoSalamancaa',
-  'https://github.com/CamiloCortesM',
-];
+import { developerData } from '@/constants';
 
 export const Contacts: FC = () => {
-  const isSmallScreen = useMediaQuery('(max-width: 600px)');
   const [open, setOpen] = useState(false);
   const handleClick = () => {
     setOpen(!open);
   };
+
   return (
     <Box width="100%">
-      {isSmallScreen ? (
-        <>
-          <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
-            <ListItemButton onClick={handleClick} sx={{ padding: '1px' }}>
-              <ListItemText
-                primary="Contactos"
-                primaryTypographyProps={{
-                  fontSize: '.9rem',
-                  color: '#714a3a',
-                  margin: '0 10px',
-                }}
-              />
-            </ListItemButton>
+      <List sx={{ width: '100%', maxWidth: 360 }} component="nav">
+        <ListItemButton onClick={handleClick} sx={{ padding: '1px' }}>
+          <ListItemText
+            primary="Contactos"
+            primaryTypographyProps={{
+              fontSize: '.9rem',
+              color: '#714a3a',
+              margin: '0 10px',
+            }}
+          />
+        </ListItemButton>
 
-            <Divider />
+        <Divider />
 
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              {developerNames.map((names, i) => (
-                <List key={i} component="div" disablePadding>
-                  <ListItemButton href={githubs[i]}>
-                    <ListItemText
-                      primary={names}
-                      primaryTypographyProps={{
-                        fontSize: '.8rem',
-                        color: '#000',
-                        margin: '0 10px',
-                      }}
-                    />
-                  </ListItemButton>
-                </List>
-              ))}
-            </Collapse>
-          </List>
-        </>
-      ) : (
-        <>
-          <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            gap={5}
-          >
-            {developerNames.map((names, i) => (
-              <Button
-                key={i}
-                sx={{
-                  padding: '0px 2px',
-                  borderRadius: '10px',
-                  backgroundColor: 'transparent',
-                  ':hover': { color: '#714a3a' },
-                  fontSize: '.8rem',
-                  color: '#000',
-                  opacity: '.8',
-                  textAlign: 'start',
-                }}
-              >
-                {names}
-              </Button>
-            ))}
-          </Box>
-        </>
-      )}
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          {developerData.map((developer, i) => (
+            <List key={i} component="div" disablePadding>
+              <ListItemButton href={developer.github}>
+                <ListItemText
+                  primary={developer.name}
+                  primaryTypographyProps={{
+                    fontSize: '.8rem',
+                    color: '#000',
+                    margin: '0 10px',
+                  }}
+                />
+              </ListItemButton>
+            </List>
+          ))}
+        </Collapse>
+      </List>
     </Box>
   );
 };
