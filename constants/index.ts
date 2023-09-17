dd725fa6ab10cd65ef8c84f6059bb0e83df026a8
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-16 19:37:29 -0500

    Feature: Add Constants File for About Component

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,5 @@ export * from './navbarConstants';
 export * from './sidebarConstants';
 export * from './userConstants';
 export * from './productConstants';
+export * from './footerConstants';
+export * from './aboutConstants';
