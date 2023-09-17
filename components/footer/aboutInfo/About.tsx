83f5cb17e4fefd036d84a19b5693d066345c4b89
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-16 19:31:20 -0500

    Refactor: Clean Up Imports, Add Props, and Organize Constants

--- components/footer/aboutInfo/About.tsx
+++ components/footer/aboutInfo/About.tsx
@@ -1,14 +1,14 @@
-import {FC} from 'react';
+import { FC } from 'react';
 
 import { Grid, Typography } from '@mui/material';
 
-export const About:FC = () => {
+export const About: FC = () => {
   return (
     <>
       <Grid item width="70%" marginY="20px">
         <Typography textAlign="center">
-          RexBuy es una plataforma de comercio electrónico diseñada por 
-          un<strong> grupo de estudiantes</strong>. Nuestra plataforma está
+          RexBuy es una plataforma de comercio electrónico diseñada por un
+          <strong> grupo de estudiantes</strong>. Nuestra plataforma está
           dedicada a brindarte la mejor experiencia de compra en línea de
           productos tecnológicos.
         </Typography>
--- components/footer/aboutInfo/ListItemBenefits.tsx
+++ components/footer/aboutInfo/ListItemBenefits.tsx
@@ -8,47 +8,25 @@ import {
   ListItemText,
   Typography,
 } from '@mui/material';
-import CategoryIcon from '@mui/icons-material/Category';
-import { AutoAwesome, Grade } from '@mui/icons-material';
+
+import { aboutElements } from '@/constants';
 
 export const ListItemBenefits: FC = () => {
-  const elementos = [
-    {
-      primary: 'Variedad de Productos',
-      secondary:
-        'Ofrecemos una amplia selección de productos tecnológicos de alta calidad.',
-      icon: <CategoryIcon />,
-    },
-
-    {
-      primary: 'Navegación Intuitiva',
-      secondary:
-        'Experimenta una navegación fácil y una experiencia de compra intuitiva en nuestra plataforma.',
-      icon: <AutoAwesome />,
-    },
-    {
-      primary: 'Productos de ultima generación',
-      secondary:
-        'Mantente actualizado con nuestras actualizaciones regulares sobre nuevos productos',
-      icon: <Grade />,
-    },
-  ];
-
   return (
     <>
       <List sx={{ width: '100%', maxWidth: 360 }}>
-        {elementos.map((elemento, i) => (
+        {aboutElements.map((element, i) => (
           <ListItem key={i} alignItems="flex-start">
             <ListItemAvatar>
               <Avatar style={{ backgroundColor: '#f7bd8f' }}>
-                {elemento.icon}
+                {element.icon}
               </Avatar>
             </ListItemAvatar>
             <ListItemText
-              primary={elemento.primary}
+              primary={element.primary}
               secondary={
                 <Typography fontSize=".8rem" variant="body1">
-                  {elemento.secondary}
+                  {element.secondary}
                 </Typography>
               }
             />
