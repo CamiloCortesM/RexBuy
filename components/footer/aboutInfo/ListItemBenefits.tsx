f7cc30a00db826f89c7fce1ec934f648c73d85dd
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-17 16:11:11 -0500

    Initialize icons in the component

--- components/footer/aboutInfo/ListItemBenefits.tsx
+++ components/footer/aboutInfo/ListItemBenefits.tsx
@@ -8,30 +8,36 @@ import {
   ListItemText,
   Typography,
 } from '@mui/material';
+import { AutoAwesome, Grade, Category } from '@mui/icons-material';
 
 import { aboutElements } from '@/constants';
+
+const elementsIcons = [Category, AutoAwesome, Grade];
 
 export const ListItemBenefits: FC = () => {
   return (
     <>
       <List sx={{ width: '100%', maxWidth: 360 }}>
-        {aboutElements.map((element, i) => (
-          <ListItem key={i} alignItems="flex-start">
-            <ListItemAvatar>
-              <Avatar style={{ backgroundColor: '#f7bd8f' }}>
-                {element.icon}
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary={element.primary}
-              secondary={
-                <Typography fontSize=".8rem" variant="body1">
-                  {element.secondary}
-                </Typography>
-              }
-            />
-          </ListItem>
-        ))}
+        {aboutElements.map((element, i) => {
+          const IconComponent = elementsIcons[i];
+          return (
+            <ListItem key={i} alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar style={{ backgroundColor: 'primary' }}>
+                  <IconComponent />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={element.primary}
+                secondary={
+                  <Typography fontSize=".8rem" variant="body1">
+                    {element.secondary}
+                  </Typography>
+                }
+              />
+            </ListItem>
+          );
+        })}
       </List>
     </>
   );
