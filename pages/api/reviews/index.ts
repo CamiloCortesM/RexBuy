c7c47606afcb37da1f9d3e301e9cecf722aeb5cb
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-09-19 23:37:20 -0500

    add create Review api and get all reviews

---
+++ pages/api/reviews/index.ts
@@ -0,0 +1,83 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/pages/api/auth/[...nextauth]';
+import { db } from '@/database';
+import { IReview } from '@/interfaces/review';
+import { Review, User } from '@/models';
+
+type Data =
+  | {
+      message: string;
+    }
+  | IReview[];
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await db.connect();
+  switch (req.method) {
+    case 'PUT':
+      return await createReviewAndMarkAsReviewed(req, res);
+    case 'GET':
+      return await getAllReviews(res);
+    default:
+      return res.status(400).json({
+        message: 'Bad request',
+      });
+  }
+};
+
+const createReviewAndMarkAsReviewed = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { product, comment = '', images = [], rating } = req.body as IReview;
+
+  const session: any = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return res
+      .status(401)
+      .json({ message: 'You must be logged in to post a review' });
+  }
+
+  const user = await User.findOne({ email: session.user.email }).lean();
+  const userId = user!._id.toString();
+
+  const review = await Review.findOne({ product, user: userId });
+
+  if (!review) {
+    return res
+      .status(400)
+      .json({ message: 'there is no review for this user or this product' });
+  }
+
+  review.comment = comment;
+  review.images = images;
+  review.rating = rating;
+
+  if (!review.reviewed) {
+    review.reviewed = true;
+  }
+
+  await review.save();
+
+  res.status(200).json({
+    message: 'review successfully created',
+  });
+};
+
+const getAllReviews = async (res: NextApiResponse<Data>) => {
+  try {
+    const reviews = await Review.find()
+      .select('rating reviewed comment product -_id')
+      .lean();
+
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error in Server',
+    });
+  }
+};
+
+export default handler;
