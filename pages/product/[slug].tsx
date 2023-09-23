9da5539d91c1425bd9c1370e5adbe939c4cb1b78
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-09-23 17:37:44 -0500

    add section to show the summary of the reviews and display them

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,47 +1,60 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
-import { Grid } from '@mui/material';
+import { Box, Divider, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../../components/layouts';
-import { ProductSlideshow } from '../../components/products';
-import { dbProducts } from '@/database';
-import { IProduct } from '@/interfaces';
+import {
+  ProductReviewCard,
+  ProductSlideshow,
+  RatingSummaryGrid,
+} from '../../components/products';
+import { dbProducts, dbReviews } from '@/database';
+import { IProduct, IReview } from '@/interfaces';
 import { ProductDetail } from '@/components/products/ProductDetail';
 
 type Props = {
   product: IProduct;
+  reviews: IReview[];
 };
 
-const ProductPage: NextPage<Props> = ({ product }) => {
+const ProductPage: NextPage<Props> = ({ product, reviews }) => {
+  const ratingCounts: { [key: number]: number } = {
+    5: 0,
+    4: 0,
+    3: 0,
+    2: 0,
+    1: 0,
+  };
+
+  for (const review of reviews) {
+    ratingCounts[review.rating]++;
+  }
+
+  const ratingPercentages: { [key: number]: number } = {};
+  for (const rating in ratingCounts) {
+    const count = ratingCounts[parseInt(rating)];
+    ratingPercentages[parseInt(rating)] = count / product.numReviewers;
+  }
+
   return (
     <ShopLayout title={product.title} pageDescription={product.description}>
-      <Grid container spacing={4} sx={{
-        padding:'25px'
-      }}>
-        <Grid item xs={12} sm={7}>
+      <Grid
+        container
+        spacing={4}
+        sx={{
+          padding: { xs: '10px 20px', md: '25px 100px' },
+        }}
+      >
+        <Grid item xs={12} md={5}>
           <ProductSlideshow images={product.images} />
-        </Grid>
-
-        <Grid item xs={12} sm={5}>
-          <ProductDetail product={product} />
-        </Grid>
-        <Grid
-          item
-          sm={6}
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            margin: '10px',
-            padding: 0,
-          }}
-        >
           {product.images.map((image, i) => (
             <img
               key={i}
               src={image}
               alt={product.title}
               style={{
-                width: '24%',
+                width: '25%',
                 maxWidth: 210,
-                maxHeight: 300,
+                height: 120,
                 objectFit: 'contain',
                 border: '1px solid rgba(0,0,0,0.05)',
                 borderRadius: 15,
@@ -49,6 +62,67 @@ const ProductPage: NextPage<Props> = ({ product }) => {
               }}
             />
           ))}
+        </Grid>
+
+        <Grid item xs={12} md={7}>
+          <ProductDetail product={product} />
+          <Divider
+            sx={{
+              margin: '30px 0',
+            }}
+          />
+          <Grid
+            container
+            sx={{
+              margin: { xs: '0px', lg: '20px 60px' },
+            }}
+          >
+            <Grid item xs={12} mb={3}>
+              <Typography variant="h2" fontWeight={500}>
+                Opiniones del producto
+              </Typography>
+            </Grid>
+            <RatingSummaryGrid
+              rating={product.rating}
+              numReviewers={product.numReviewers}
+              ratingPercentages={ratingPercentages}
+            />
+            <Grid
+              item
+              xs={12}
+              lg={8}
+              px={{ xs: 0, lg: 2 }}
+              py={{ xs: 2, lg: 0 }}
+            >
+              {reviews.length > 0 ? (
+                reviews.map((review) => (
+                  <ProductReviewCard
+                    key={review._id}
+                    comment={review.comment}
+                    dateReview={review.updatedAt!}
+                    rating={review.rating}
+                    images={review.images}
+                  />
+                ))
+              ) : (
+                <Box
+                  width="100%"
+                  height={200}
+                  sx={{
+                    backgroundColor: '#ececec',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <Typography textAlign="center" variant="body2" width={300}>
+                    ¡Adquiere este increíble producto y sé el primero en dejar
+                    tu opinión!
+                  </Typography>
+                </Box>
+              )}
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </ShopLayout>
@@ -80,11 +154,15 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       },
     };
   }
+
+  const reviews: IReview[] = await dbReviews.getReviewsByProduct(product._id);
+
   return {
     props: {
       product,
+      reviews,
     },
-    // revalidate: 60 * 60 * 24,
+    // revalidate: 60 * 60 * 24, for better seo
     revalidate: 60,
   };
 };
