4b240b852626cffc2065f94dbc171fbafea441fb
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-24 14:30:04 -0500

    Create UserImage component with styles

---
+++ components/profile/UserImage.tsx
@@ -0,0 +1,113 @@
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+
+import { Avatar, Badge, Button, Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import { rexbuyApi } from '@/api';
+import { IUser } from '@/interfaces';
+import style from './Successfully.module.css';
+
+type Props = {
+  user: IUser;
+  updateUser: Function;
+};
+
+export const UserImage: FC<Props> = ({ user, updateUser }) => {
+  const [selectImage, setSelectImage] = useState<File | null>(null);
+  const { update } = useSession();
+  const [successfully, setSuccessfully] = useState(false);
+
+  useEffect(() => {}, [user.userImage]);
+
+  const SmallAvatar = styled(Avatar)(({ theme }) => ({
+    width: 25,
+    height: 25,
+    border: `1px solid ${theme.palette.background.paper}`,
+    padding: '2px',
+  }));
+
+  const handleImageChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setSelectImage(e.target.files[0]);
+
+    const formData = new FormData();
+    formData.append('file', e.target.files[0]);
+
+    try {
+      setSuccessfully(true);
+      const { data } = await rexbuyApi.post<{ message: string }>(
+        '/upload',
+        formData
+      );
+
+      const info = { imageUrl: data.message };
+      const { data: dataUser } = await rexbuyApi.post<{ updatedUser: IUser }>(
+        '/profile/edit-user-image',
+        info
+      );
+
+      const user: IUser = dataUser.updatedUser;
+      const { email } = user;
+
+      await updateUser(user);
+      await update({ email });
+      setSuccessfully(false);
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  return (
+    <>
+      <Button
+        className={successfully ? `${style.pulse} ${style.circle}` : ''}
+        component="label"
+        style={{ height: '62px', borderRadius: '50%', width: '62px' }}
+      >
+        <input
+          type="file"
+          accept="image/png, image/jpeg, image/webp, image/jpg"
+          hidden
+          onChange={handleImageChange}
+        />
+
+        <Stack
+          direction="row"
+          spacing={2}
+          height="72px"
+          width="72px"
+          sx={{
+            border: '1px solid rgba(0, 0, 0, 0.12)',
+            borderRadius: '50%',
+            backgroundColor: '#fff',
+          }}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Badge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            badgeContent={
+              <SmallAvatar
+                alt="Add Image"
+                src="/profile/camera.svg"
+                style={{ backgroundColor: '#fff' }}
+              />
+            }
+          >
+            <Avatar
+              alt="User Image"
+              src={
+                selectImage
+                  ? URL.createObjectURL(selectImage)
+                  : user.userImage || '/profile/default-profile.svg'
+              }
+              sx={{ width: 70, height: 70 }}
+            />
+          </Badge>
+        </Stack>
+      </Button>
+    </>
+  );
+};
