35c16be57601bbbaf4168b0246d2b5507fc11ed1
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-24 14:15:37 -0500

    Add '[User] - update' action to authReducer

--- context/auth/authReducer.ts
+++ context/auth/authReducer.ts
@@ -3,13 +3,13 @@ import { AuthState } from './';
 
 type AuthActionType =
   | { type: '[Auth] - Login'; payload: IUser }
-  | { type: '[Auth] - Logout'; payload: IUser };
+  | { type: '[Auth] - Logout'; payload: IUser }
+  | { type: '[User] - Update'; payload: IUser };
 
 export const authReducer = (
   state: AuthState,
   action: AuthActionType
 ): AuthState => {
-  
   switch (action.type) {
     case '[Auth] - Login':
       return {
@@ -23,6 +23,12 @@ export const authReducer = (
         ...state,
         isLoggedIn: false,
         user: undefined,
+      };
+
+    case '[User] - Update':
+      return {
+        ...state,
+        user: action.payload,
       };
 
     default:
