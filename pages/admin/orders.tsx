eb27d9785ec752336fe8ff7de106563af3098f6c
DANIELSSF <daniel.santiago730@gmail.com>
2023-09-24 17:27:28 -0500

    Update order info

--- pages/admin/orders.tsx
+++ pages/admin/orders.tsx
@@ -1,14 +1,31 @@
 import useSWR from 'swr';
-import { Chip, Grid } from '@mui/material';
+import { Avatar, Chip, Grid } from '@mui/material';
 import { ConfirmationNumberOutlined } from '@mui/icons-material';
-import { DataGrid, GridColDef, GridRenderCellParams, GridToolbar } from '@mui/x-data-grid';
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridToolbar,
+} from '@mui/x-data-grid';
 import { AdminLayout } from '@/components/layouts';
 import { IOrder, IUser } from '@/interfaces';
+import { formatDate } from '@/utils/date';
+
+const defaultImage = '/profile/default-profile.svg';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'Orden ID', width: 250 },
+  {
+    field: 'avatar',
+    headerName: 'Avatar',
+    width: 60,
+    renderCell: ({ row }: GridRenderCellParams) => {
+      const userImage = row.avatar || defaultImage;
+      return <Avatar src={userImage} alt="user-image" />;
+    },
+  },
+  { field: 'name', headerName: 'Cliente', width: 250 },
   { field: 'email', headerName: 'Correo', width: 250 },
-  { field: 'name', headerName: 'Nombre Completo', width: 250 },
   { field: 'total', headerName: 'Monto total', minWidth: 150 },
   {
     field: 'isPaid',
@@ -48,14 +65,17 @@ const OrdersPage = () => {
 
   if ((!data && !error) || isLoading) return <></>;
 
+  console.log(data);
+
   const rows = data!.map((order) => ({
-    id        : order._id,
-    email     : (order.user as IUser).email,
-    name      : (order.user as IUser).name,
-    total     : order.total,
-    isPaid    : order.isPaid,
+    id: order._id,
+    email: (order.user as IUser).email,
+    avatar: (order.user as IUser).userImage,
+    name: (order.user as IUser).name,
+    total: order.total,
+    isPaid: order.isPaid,
     noProducts: order.numberOfItems,
-    createdAt : order.createdAt,
+    createdAt: formatDate(order.createdAt!),
   }));
   return (
     <AdminLayout
